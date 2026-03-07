be1ecd904dd3d0e53b9a2e437a638ce1d25b7c9e
Mitchell Brovarnik <mitchellbrovarnik@gmail.com>
2026-03-06 19:23:42 -0500

    ci: fix linux deb author constraint and github builder release type restriction

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -5,23 +5,27 @@ module.exports = {
         app: 'dist',
         output: 'release'
     },
+    publish: {
+        provider: 'github',
+        releaseType: 'release'
+    },
     files: ['**/*'],
     win: {
         target: 'nsis',
-        icon: 'dist/icon.ico'
+        icon: 'dist/assets/icon.ico'
     },
     nsis: {
         oneClick: false,
         allowToChangeInstallationDirectory: true,
-        installerHeaderIcon: 'dist/icon.ico'
+        installerHeaderIcon: 'dist/assets/icon.ico'
     },
     mac: {
         target: 'dmg',
-        icon: 'dist/icon.icns'
+        icon: 'dist/assets/icon.icns'
     },
     linux: {
         target: ['AppImage', 'deb'],
-        icon: 'dist/icon.png',
+        icon: 'dist/assets/icon.png',
         category: 'Audio'
     }
 };
