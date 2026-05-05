05016abe288279e246aa3d750a68f175c21f0d4d
Mitchell Brovarnik <mitchellbrovarnik@gmail.com>
2026-05-05 12:27:19 -0400

    fix: Increased SVG scaling in Library card so icon matches album art size

--- renderer.js
+++ renderer.js
@@ -413,7 +413,7 @@ function renderLibraryPage() {
 
     // Always pin the Shuffle Stations card to the top if it matches the filter
     if (!filterQuery || 'shuffle'.includes(filterQuery.toLowerCase())) {
-        const shuffleIconUrl = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 32 32'%3E%3Cpath fill='%23ffffff' opacity='0.9' d='M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z'/%3E%3C/svg%3E";
+        const shuffleIconUrl = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='2 2 20 20'%3E%3Cpath fill='%23ffffff' opacity='0.9' d='M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z'/%3E%3C/svg%3E";
         cardsHtml += `
             <div class="card" id="library-shuffle-card" tabindex="0">
               <div class="card-image-container">
